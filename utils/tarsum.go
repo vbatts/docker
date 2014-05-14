48b42814c61ff26e202eaa0936ffc4234e9f80ab
Vincent Batts <vbatts@redhat.com>
2014-05-13 23:45:37 -0400

    tarsum: cleanup, testing and benchmarks
    rather than committing a 1mb file, have a test that produces one.
    Also, benchmark TarSum for a 9k and 1mb layer (w/ and w/o gzip)
    
    Docker-DCO-1.1-Signed-off-by: Vincent Batts <vbatts@redhat.com> (github: vbatts)

--- utils/tarsum.go
+++ utils/tarsum.go
@@ -13,13 +13,19 @@ import (
 	"strings"
 )
 
+/*
+A wrapper for an io.Reader that is an archive/tar stream, and provides an
+io.Reader that is useable by archive/tar NewReader
+
+Along the way generates a fix-time sha256 hash of the contents of the incoming tar.
+*/
 type TarSum struct {
 	io.Reader
 	tarR               *tar.Reader
 	tarW               *tar.Writer
-	gz                 writeCloseFlusher
+	writer             writeCloseFlusher
 	bufTar             *bytes.Buffer
-	bufGz              *bytes.Buffer
+	bufWriter          *bytes.Buffer
 	h                  hash.Hash
 	sums               map[string]string
 	currentFile        string
@@ -69,71 +75,76 @@ func (ts *TarSum) encodeHeader(h *tar.Header) error {
 	return nil
 }
 
+func (ts *TarSum) initTarSum() error {
+	ts.bufTar = bytes.NewBuffer([]byte{})
+	ts.bufWriter = bytes.NewBuffer([]byte{})
+	ts.tarR = tar.NewReader(ts.Reader)
+	ts.tarW = tar.NewWriter(ts.bufTar)
+	if !ts.DisableCompression {
+		ts.writer = gzip.NewWriter(ts.bufWriter)
+	} else {
+		ts.writer = &nopCloseFlusher{Writer: ts.bufWriter}
+	}
+	ts.h = sha256.New()
+	ts.h.Reset()
+	ts.first = true
+	ts.sums = make(map[string]string)
+	return nil
+}
+
 func (ts *TarSum) Read(buf []byte) (int, error) {
-	if ts.gz == nil {
-		ts.bufTar = bytes.NewBuffer([]byte{})
-		ts.bufGz = bytes.NewBuffer([]byte{})
-		ts.tarR = tar.NewReader(ts.Reader)
-		ts.tarW = tar.NewWriter(ts.bufTar)
-		if !ts.DisableCompression {
-			ts.gz = gzip.NewWriter(ts.bufGz)
-		} else {
-			ts.gz = &nopCloseFlusher{Writer: ts.bufGz}
+	if ts.writer == nil {
+		if err := ts.initTarSum(); err != nil {
+			return 0, err
 		}
-		ts.h = sha256.New()
-		ts.h.Reset()
-		ts.first = true
-		ts.sums = make(map[string]string)
 	}
 
 	if ts.finished {
-		return ts.bufGz.Read(buf)
+		return ts.bufWriter.Read(buf)
 	}
 	buf2 := make([]byte, len(buf), cap(buf))
 
 	n, err := ts.tarR.Read(buf2)
-	if err != nil {
-		if err == io.EOF {
-			if _, err := ts.h.Write(buf2[:n]); err != nil {
-				return 0, err
-			}
-			if !ts.first {
-				ts.sums[ts.currentFile] = hex.EncodeToString(ts.h.Sum(nil))
-				ts.h.Reset()
-			} else {
-				ts.first = false
-			}
-
-			currentHeader, err := ts.tarR.Next()
-			if err != nil {
-				if err == io.EOF {
-					if err := ts.gz.Close(); err != nil {
-						return 0, err
-					}
-					ts.finished = true
-					return n, nil
-				}
-				return n, err
-			}
-			ts.currentFile = strings.TrimSuffix(strings.TrimPrefix(currentHeader.Name, "./"), "/")
-			if err := ts.encodeHeader(currentHeader); err != nil {
-				return 0, err
-			}
-			if err := ts.tarW.WriteHeader(currentHeader); err != nil {
-				return 0, err
-			}
-			if _, err := ts.tarW.Write(buf2[:n]); err != nil {
-				return 0, err
-			}
-			ts.tarW.Flush()
-			if _, err := io.Copy(ts.gz, ts.bufTar); err != nil {
-				return 0, err
-			}
-			ts.gz.Flush()
-
-			return ts.bufGz.Read(buf)
-		}
+	if err != nil && err != io.EOF {
 		return n, err
+	} else if err != nil {
+		if _, err := ts.h.Write(buf2[:n]); err != nil {
+			return 0, err
+		}
+		if !ts.first {
+			ts.sums[ts.currentFile] = hex.EncodeToString(ts.h.Sum(nil))
+			ts.h.Reset()
+		} else {
+			ts.first = false
+		}
+
+		currentHeader, err := ts.tarR.Next()
+		if err != nil && err == io.EOF {
+			if err := ts.writer.Close(); err != nil {
+				return 0, err
+			}
+			ts.finished = true
+			return n, nil
+		} else if err != nil {
+			return n, err
+		}
+		ts.currentFile = strings.TrimSuffix(strings.TrimPrefix(currentHeader.Name, "./"), "/")
+		if err := ts.encodeHeader(currentHeader); err != nil {
+			return 0, err
+		}
+		if err := ts.tarW.WriteHeader(currentHeader); err != nil {
+			return 0, err
+		}
+		if _, err := ts.tarW.Write(buf2[:n]); err != nil {
+			return 0, err
+		}
+		ts.tarW.Flush()
+		if _, err := io.Copy(ts.writer, ts.bufTar); err != nil {
+			return 0, err
+		}
+		ts.writer.Flush()
+
+		return ts.bufWriter.Read(buf)
 	}
 
 	// Filling the hash buffer
@@ -147,13 +158,13 @@ func (ts *TarSum) Read(buf []byte) (int, error) {
 	}
 	ts.tarW.Flush()
 
-	// Filling the gz writter
-	if _, err = io.Copy(ts.gz, ts.bufTar); err != nil {
+	// Filling the output writer
+	if _, err = io.Copy(ts.writer, ts.bufTar); err != nil {
 		return 0, err
 	}
-	ts.gz.Flush()
+	ts.writer.Flush()
 
-	return ts.bufGz.Read(buf)
+	return ts.bufWriter.Read(buf)
 }
 
 func (ts *TarSum) Sum(extra []byte) string {
--- utils/tarsum_test.go
+++ utils/tarsum_test.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"bytes"
+	"crypto/rand"
+	"github.com/dotcloud/docker/vendor/src/code.google.com/p/go/src/pkg/archive/tar"
 	"io"
 	"io/ioutil"
 	"os"
@@ -9,6 +12,7 @@ import (
 
 type testLayer struct {
 	filename string
+	reader   io.Reader
 	jsonfile string
 	gzip     bool
 	tarsum   string
@@ -20,19 +24,75 @@ var testLayers = []testLayer{
 		jsonfile: "testdata/46af0962ab5afeb5ce6740d4d91652e69206fc991fd5328c1a94d364ad00e457/json",
 		tarsum:   "tarsum+sha256:e58fcf7418d4390dec8e8fb69d88c06ec07039d651fedd3aa72af9972e7d046b"},
 	{
+		filename: "testdata/46af0962ab5afeb5ce6740d4d91652e69206fc991fd5328c1a94d364ad00e457/layer.tar",
+		jsonfile: "testdata/46af0962ab5afeb5ce6740d4d91652e69206fc991fd5328c1a94d364ad00e457/json",
+		gzip:     true,
+		tarsum:   "tarsum+sha256:e58fcf7418d4390dec8e8fb69d88c06ec07039d651fedd3aa72af9972e7d046b"},
+	{
 		filename: "testdata/511136ea3c5a64f264b78b5433614aec563103b4d4702f3ba7d4d2698e22c158/layer.tar",
 		jsonfile: "testdata/511136ea3c5a64f264b78b5433614aec563103b4d4702f3ba7d4d2698e22c158/json",
 		tarsum:   "tarsum+sha256:ac672ee85da9ab7f9667ae3c32841d3e42f33cc52c273c23341dabba1c8b0c8b"},
+	{
+		reader: sizedTar(1024*1024, false), // a 1mb file,
+		tarsum: "tarsum+sha256:6a239fc02a5ed61f07ccb88ba2d59979778a77e9d0976bf61162d65e0ba5cfa8"},
+}
+
+func sizedTar(size int64, isRand bool) io.Reader {
+	b := bytes.NewBuffer([]byte{})
+	tarW := tar.NewWriter(b)
+	err := tarW.WriteHeader(&tar.Header{
+		Name: "/testdata",
+		Mode: 0755,
+		Uid:  0,
+		Gid:  0,
+		Size: size,
+	})
+	if err != nil {
+		return nil
+	}
+	var rBuf []byte
+	if isRand {
+		rBuf = make([]byte, 8)
+		_, err = rand.Read(rBuf)
+		if err != nil {
+			return nil
+		}
+	} else {
+		rBuf = []byte{0, 0, 0, 0, 0, 0, 0, 0}
+	}
+
+	for i := int64(0); i < size/int64(8); i++ {
+		tarW.Write(rBuf)
+	}
+	return b
 }
 
 func TestTarSums(t *testing.T) {
 	for _, layer := range testLayers {
-		fh, err := os.Open(layer.filename)
-		if err != nil {
-			t.Errorf("failed to open %s: %s", layer.filename, err)
+		var (
+			fh  io.Reader
+			err error
+		)
+		if len(layer.filename) > 0 {
+			t.Log(os.Getwd())
+			fh, err = os.Open(layer.filename)
+			if err != nil {
+				t.Errorf("failed to open %s: %s", layer.filename, err)
+				continue
+			}
+		} else if layer.reader != nil {
+			fh = layer.reader
+		} else {
+			// What else is there to test?
+			t.Errorf("what to do with %#V", layer)
 			continue
 		}
-		ts := &TarSum{Reader: fh}
+		if file, ok := fh.(*os.File); ok {
+			defer file.Close()
+		}
+
+		//                                  double negatives!
+		ts := &TarSum{Reader: fh, DisableCompression: !layer.gzip}
 		_, err = io.Copy(ioutil.Discard, ts)
 		if err != nil {
 			t.Errorf("failed to copy from %s: %s", layer.filename, err)
@@ -58,6 +118,77 @@ func TestTarSums(t *testing.T) {
 		if layer.tarsum != gotSum {
 			t.Errorf("expecting [%s], but got [%s]", layer.tarsum, gotSum)
 		}
-
 	}
 }
+
+func Benchmark9kFile(b *testing.B) {
+	buf := bytes.NewBuffer([]byte{})
+	fh, err := os.Open("testdata/46af0962ab5afeb5ce6740d4d91652e69206fc991fd5328c1a94d364ad00e457/layer.tar")
+	if err != nil {
+		b.Error(err)
+		return
+	}
+	n, err := io.Copy(buf, fh)
+	fh.Close()
+
+	b.SetBytes(n)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ts := &TarSum{Reader: buf, DisableCompression: true}
+		io.Copy(ioutil.Discard, ts)
+		ts.Sum(nil)
+	}
+}
+
+func Benchmark9kFileGzip(b *testing.B) {
+	buf := bytes.NewBuffer([]byte{})
+	fh, err := os.Open("testdata/46af0962ab5afeb5ce6740d4d91652e69206fc991fd5328c1a94d364ad00e457/layer.tar")
+	if err != nil {
+		b.Error(err)
+		return
+	}
+	n, err := io.Copy(buf, fh)
+	fh.Close()
+
+	b.SetBytes(n)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ts := &TarSum{Reader: buf, DisableCompression: false}
+		io.Copy(ioutil.Discard, ts)
+		ts.Sum(nil)
+	}
+}
+
+// this is a single big file in the tar archive
+func Benchmark1mbFile(b *testing.B) {
+	var buf *bytes.Buffer
+	tarReader := sizedTar(1024*1024, true)
+	if br, ok := tarReader.(*bytes.Buffer); ok {
+		buf = br
+	}
+	b.SetBytes(1024 * 1024)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ts := &TarSum{Reader: buf, DisableCompression: true}
+		io.Copy(ioutil.Discard, ts)
+		ts.Sum(nil)
+	}
+}
+
+// this is a single big file in the tar archive
+func Benchmark1mbFileGzip(b *testing.B) {
+	var buf *bytes.Buffer
+	tarReader := sizedTar(1024*1024, true)
+	if br, ok := tarReader.(*bytes.Buffer); ok {
+		buf = br
+	}
+	b.SetBytes(1024 * 1024)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ts := &TarSum{Reader: buf, DisableCompression: false}
+		io.Copy(ioutil.Discard, ts)
+		ts.Sum(nil)
+	}
+}
+
+// TODO make a tar archive full of sparse empty files
