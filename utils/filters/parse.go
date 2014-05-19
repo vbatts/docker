c2b5903b556a9af9b113e362f3ca248865aee9b2
Vincent Batts <vbatts@redhat.com>
2014-05-19 16:31:15 -0400

    filter flag: split out for separate --filter flags
    adding tests and allowing for easy passing of filters.Args from client
    to server.
    
    Docker-DCO-1.1-Signed-off-by: Vincent Batts <vbatts@redhat.com> (github: vbatts)

--- utils/filters/parse.go
+++ utils/filters/parse.go
@@ -2,46 +2,57 @@ package filters
 
 import (
 	"errors"
+	"github.com/dotcloud/docker/pkg/beam/data"
 	"strings"
 )
+
+var ArgPrefix = "filter."
+
+type Args map[string][]string
 
 /*
 Parse the argument to the filter flag. Like
 
-  `docker ps -f 'created=today;image.name=ubuntu*'`
-
-Filters delimited by ';', and expected to be 'name=value'
+  `docker ps -f 'created=today' -f 'image.name=ubuntu*'`
 
 If prev map is provided, then it is appended to, and returned. By default a new
 map is created.
 */
-func ParseFlag(arg string, prev map[string]string) (map[string]string, error) {
-	var filters map[string]string
-	if prev != nil {
-		filters = prev
-	} else {
-		filters = map[string]string{}
+func ParseFlag(arg string, prev Args) (Args, error) {
+	var filters Args = prev
+	if prev == nil {
+		filters = Args{}
 	}
 	if len(arg) == 0 {
 		return filters, nil
 	}
 
-	for _, chunk := range strings.Split(arg, ";") {
-		if !strings.Contains(chunk, "=") {
-			return filters, ErrorBadFormat
-		}
-		f := strings.SplitN(chunk, "=", 2)
-		filters[f[0]] = f[1]
+	if !strings.Contains(arg, "=") {
+		return filters, ErrorBadFormat
 	}
+
+	f := strings.SplitN(arg, "=", 2)
+	filters[f[0]] = append(filters[f[0]], f[1])
+
 	return filters, nil
 }
 
 var ErrorBadFormat = errors.New("bad format of filter (expected name=value)")
 
-func ToParam(f map[string]string) string {
-	fs := []string{}
-	for k, v := range f {
-		fs = append(fs, k+"="+v)
-	}
-	return strings.Join(fs, ";")
+/*
+merges the Args into an existing url.Values and returns that product
+
+This appends a "filter." prefix to the keys, to not conflict with existing values
+*/
+func ToParam(a Args) string {
+	return data.Encode(a)
+}
+
+/*
+from a url.Values, extract the filter Args
+
+This is based on a "filter." prefix of the keys
+*/
+func FromParam(p string) (Args, error) {
+	return data.Decode(p)
 }
