9026e8d4d5ce05639a26e5eed8725d4e384a39a6
Vincent Batts <vbatts@redhat.com>
2014-10-01 13:29:56 -0400

    mount: allow for shared, slave and recursive
    Signed-off-by: Vincent Batts <vbatts@redhat.com>

--- pkg/mount/flags_freebsd.go
+++ pkg/mount/flags_freebsd.go
@@ -20,6 +20,11 @@ const (
 	NODEV       = 0
 	NODIRATIME  = 0
 	PRIVATE     = 0
+	RPRIVATE    = 0
+	SHARED      = 0
+	RSHARED     = 0
+	SLAVE       = 0
+	RSLAVE      = 0
 	RBIND       = 0
 	RELATIVE    = 0
 	RELATIME    = 0
--- pkg/mount/flags_linux.go
+++ pkg/mount/flags_linux.go
@@ -18,6 +18,11 @@ const (
 	BIND        = syscall.MS_BIND
 	RBIND       = syscall.MS_BIND | syscall.MS_REC
 	PRIVATE     = syscall.MS_PRIVATE
+	RPRIVATE    = syscall.MS_PRIVATE | syscall.MS_REC
+	SLAVE       = syscall.MS_SLAVE
+	RSLAVE      = syscall.MS_SLAVE | syscall.MS_REC
+	SHARED      = syscall.MS_SHARED
+	RSHARED     = syscall.MS_SHARED | syscall.MS_REC
 	RELATIME    = syscall.MS_RELATIME
 	STRICTATIME = syscall.MS_STRICTATIME
 )
