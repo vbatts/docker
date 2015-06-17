851b2a5c2ed28ee1a3def479cffc833dd31b09fd
Dan Walsh <dwalsh@redhat.com>
2015-06-17 07:59:41 -0400

    Super Secrets Patch
    Docker-DCO-1.1-Signed-off-by: Dan Walsh <dwalsh@redhat.com> (github: rhatdan)
    
    Dan Walsh <dwalsh@redhat.com>

--- daemon/container_linux.go
+++ daemon/container_linux.go
@@ -28,6 +28,7 @@ import (
 	"github.com/docker/docker/utils"
 	"github.com/docker/libcontainer/configs"
 	"github.com/docker/libcontainer/devices"
+	"github.com/docker/libcontainer/label"
 	"github.com/docker/libnetwork"
 	"github.com/docker/libnetwork/netlabel"
 	"github.com/docker/libnetwork/options"
@@ -1026,6 +1027,33 @@ func (container *Container) UnmountVolumes(forceSyscall bool) error {
 				return err
 			}
 		}
+	}
+
+	return nil
+}
+func (container *Container) secretsPath() (string, error) {
+	return container.GetRootResourcePath("secrets")
+}
+func (container *Container) setupSecretFiles() error {
+	secretsPath, err := container.secretsPath()
+	if err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(secretsPath, 0700); err != nil {
+		return err
+	}
+
+	if err := syscall.Mount("tmpfs", secretsPath, "tmpfs", uintptr(syscall.MS_NOEXEC|syscall.MS_NOSUID|syscall.MS_NODEV), label.FormatMountLabel("", container.GetMountLabel())); err != nil {
+		return fmt.Errorf("mounting secret tmpfs: %s", err)
+	}
+
+	data, err := getHostSecretData()
+	if err != nil {
+		return err
+	}
+	for _, s := range data {
+		s.SaveTo(secretsPath)
 	}
 
 	return nil
